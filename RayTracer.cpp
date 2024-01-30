5605a17f4cc0d661e9b80c51dea141ec74671d54
Filip Mikoviny <filip.mikoviny@epfl.ch>
2024-01-31 00:19:43 +0100

    basics, ray generation almost done

--- RayTracer.cpp
+++ RayTracer.cpp
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <limits>
 #include <cmath>
 #include <iostream>
@@ -5,29 +6,57 @@
 #include <vector>
 #include "geometry.h"
 
-void render() {
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+void render(Sphere sphere) {
     const int width = 1024;
     const int height = 768;
-    std::vector<Vec3f> framebuffer(width * height);
+    const int channels = 3;
+    const int byte_stride = width;
+    const Vec3f origin = Vec3f(0.0, 0.0, 0.0);
+    const float screen_cam_dist = 1.0;
+    const float fov_angle = 90.;
+    const float aspect = width / height;
+
+    std::vector<Vec3uc> framebuffer(width * height);
+    //unsigned char* test = new unsigned char[width * height * channels];
 
     for (size_t j = 0; j < height; j++) {
         for (size_t i = 0; i < width; i++) {
-            framebuffer[i + j * width] = Vec3f(j / float(height), i / float(width), 0);
+            int r = int(j / float(height) * 255);
+            int g = int(i / float(width) * 255);
+            int b = 0;
+            framebuffer[i + j * width] = Vec3uc(r, g, b);
+
+            float screen_width = tan(fov / 2.) * 2 * screen_cam_dist;
+            float rx = (i + 0.5) / width * screen_width * aspect;
+            float ry = (j + 0.5) / height * screen_width;
+            float rz = -1.; //Camera is looking in negative Z direction
+            //test[(i + j * width) * 3] = r;
+            //test[(i + j * width) * 3 + 1] = g;
+            //test[(i + j * width) * 3 + 2] = b;
         }
     }
 
-    std::ofstream ofs; // save the framebuffer to file
-    ofs.open("./out.ppm");
-    ofs << "P6\n" << width << " " << height << "\n255\n";
-    for (size_t i = 0; i < height * width; ++i) {
-        for (size_t j = 0; j < 3; j++) {
-            ofs << (char)(255 * std::max(0.f, std::min(1.f, framebuffer[i][j])));
-        }
-    }
-    ofs.close();
+    stbi_write_bmp("output.bmp", width, height, channels, framebuffer.data());
+
+    delete(test);
+
+    std::cout << "Done" << std::endl;
+
 }
 
 int main() {
-    render();
+    Sphere sphere = Sphere(Vec3f(0.8, 0.3, -4.0), 4.0);
+    render(sphere);
     return 0;
+}
+
+void ray_loop(Vec3f origin, float screen_dist, float fov) {
+
+}
+
+Vec3f cast_ray(Vec3f ro, Vec3f rd) {
+
 }
--- geometry.h
+++ geometry.h
@@ -1,19 +1,26 @@
 #pragma once
 
 #include <cassert>
+#include <cmath>
+#include <vector>
+#include <iostream>
 
 
 template <size_t DIM, typename T>
 struct vec{
-	vec() {
-		for (size_t i = 0; i < DIM; i++) {
-			data[i] = T();
-		}
-		T& operator[](const size_t i) {
-			assert(i < DIM);
-			return data[i];
-		}
-	}
+    vec() {
+        for (size_t i = 0; i < DIM; i++) {
+            data[i] = T();
+        }
+    }
+    T& operator[](const size_t i) {
+        assert(i < DIM);
+        return data[i];
+    }
+    const T& operator[](const size_t i) {
+        assert(i < DIM);
+        return data[i];
+    }
 private:
 	T data[DIM];
 };
@@ -21,10 +28,12 @@ private:
 typedef vec<2, float> Vec2f;
 typedef vec<3, float> Vec3f;
 typedef vec<3, int  > Vec3i;
+typedef vec<3, unsigned char> Vec3uc;
 typedef vec<4, float> Vec4f;
 
 
-template <typename T> struct vec<2, T> {
+template <typename T> 
+struct vec<2, T> {
     vec() : x(T()), y(T()) {}
     vec(T X, T Y) : x(X), y(Y) {}
     template <class U> vec<2, T>(const vec<2, U>& v);
@@ -33,7 +42,8 @@ template <typename T> struct vec<2, T> {
     T x, y;
 };
 
-template <typename T> struct vec<3, T> {
+template <typename T> 
+struct vec<3, T> {
     vec() : x(T()), y(T()), z(T()) {}
     vec(T X, T Y, T Z) : x(X), y(Y), z(Z) {}
     T& operator[](const size_t i) { assert(i < 3); return i <= 0 ? x : (1 == i ? y : z); }
@@ -43,10 +53,86 @@ template <typename T> struct vec<3, T> {
     T x, y, z;
 };
 
-template <typename T> struct vec<4, T> {
+template <typename T> 
+struct vec<4, T> {
     vec() : x(T()), y(T()), z(T()), w(T()) {}
     vec(T X, T Y, T Z, T W) : x(X), y(Y), z(Z), w(W) {}
     T& operator[](const size_t i) { assert(i < 4); return i <= 0 ? x : (1 == i ? y : (2 == i ? z : w)); }
     const T& operator[](const size_t i) const { assert(i < 4); return i <= 0 ? x : (1 == i ? y : (2 == i ? z : w)); }
     T x, y, z, w;
 };
+
+//Dot product
+template<size_t DIM, typename T>
+T operator*(const vec<DIM, T>& lhs, const vec<DIM, T>& rhs) {
+    T ret = T();
+    for (size_t i = 0; i < DIM; i++) {
+        ret += lhs[i] * rhs[i];
+    }
+    return ret;
+}
+
+//Addition
+template<size_t DIM, typename T>
+vec<DIM, T> operator+(const vec<DIM, T>& lhs, const vec<DIM, T>& rhs) {
+    for (size_t i = 0; i < DIM; i++) {
+        lhs[i] = lhs[i] + rhs[i];
+    }
+    return lhs;
+}
+
+//Subtraction
+template<size_t DIM, typename T>
+vec<DIM, T> operator-(const vec<DIM, T>& lhs, const vec<DIM, T>& rhs) {
+    for (size_t i = 0; i < DIM; i++) {
+        lhs[i] = lhs[i] - rhs[i];
+    }
+    return lhs;
+}
+
+//Scaling
+template<size_t DIM, typename T, typename U>
+vec<DIM, T> operator*(const vec<DIM, T>& lhs, const U& rhs) {
+    for (size_t i = 0; i < DIM; i++) {
+        lhs[i] = lhs[i] * rhs;
+    }
+    return lhs;
+}
+
+//Negation
+template<size_t DIM, typename T>
+vec<DIM, T> operator-(const vec<DIM, T>& lhs) {
+    return lhs * (T(-1));
+}
+
+//Cross product
+template <typename T> vec<3, T> cross(vec<3, T> v1, vec<3, T> v2) {
+    return vec<3, T>(v1.y * v2.z - v1.z * v2.y, v1.z * v2.x - v1.x * v2.z, v1.x * v2.y - v1.y * v2.x);
+}
+
+
+
+
+
+
+
+struct Sphere {
+    Vec3f center;
+    float radius;
+
+    Sphere(const Vec3f& c, const float& r) : center(c), radius(r) {}
+
+    bool ray_intersect(const Vec3f& orig, const Vec3f& dir, float& t0) const {
+        Vec3f L = center - orig;
+        float tca = L * dir;
+        float d2 = L * L - tca * tca;
+        if (d2 > radius * radius) return false;
+        float thc = sqrtf(radius * radius - d2);
+        t0 = tca - thc;
+        float t1 = tca + thc;
+        if (t0 < 0) t0 = t1;
+        if (t0 < 0) return false;
+        return true;
+    }
+};
+
